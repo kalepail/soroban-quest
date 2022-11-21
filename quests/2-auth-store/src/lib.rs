15824038a3ea05f9ba34bcc590a296180bb290d7
Tyler van der Hoeven <hi@tyvdh.com>
2022-11-21 11:02:42 -0500

    uncommenting `get_self` so `make test` works again

--- quests/2-auth-store/src/lib.rs
+++ quests/2-auth-store/src/lib.rs
@@ -57,24 +57,24 @@ impl DataStoreContract {
     // TODO Let's make sure someone uncomments this 👇 function before Q2 goes live or everyone will Nesho it
     // !!!
 
-    // /// The `get_self()` function works similarly to `get()`, except `owner` is
-    // /// omitted. The AccountId to retrieve associated data for is supplied using
-    // /// a call to `env.invoker()`. Again we don't allow cross-contract
-    // /// invokations of this function. If there is no data associated, return
-    // /// Bytes of length 0.
-    // pub fn get_self(env: Env) -> Result<Bytes, ContractError> {
-    //     let key = match env.invoker() {
-    //         Address::Account(account_id) => account_id,
-    //         Address::Contract(_) => {
-    //             panic_with_error!(&env, ContractError::CrossContractCallProhibited)
-    //         }
-    //     };
-    //     Ok(env
-    //         .data()
-    //         .get(key)
-    //         .unwrap_or_else(|| Ok(bytes!(&env)))
-    //         .unwrap())
-    // }
+    /// The `get_self()` function works similarly to `get()`, except `owner` is
+    /// omitted. The AccountId to retrieve associated data for is supplied using
+    /// a call to `env.invoker()`. Again we don't allow cross-contract
+    /// invokations of this function. If there is no data associated, return
+    /// Bytes of length 0.
+    pub fn get_self(env: Env) -> Result<Bytes, ContractError> {
+        let key = match env.invoker() {
+            Address::Account(account_id) => account_id,
+            Address::Contract(_) => {
+                panic_with_error!(&env, ContractError::CrossContractCallProhibited)
+            }
+        };
+        Ok(env
+            .data()
+            .get(key)
+            .unwrap_or_else(|| Ok(bytes!(&env)))
+            .unwrap())
+    }
 }
 
 mod error;
