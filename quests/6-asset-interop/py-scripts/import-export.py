61ce0ae64dffbbd3a08e7ef990f408a14d646dc1
overcat <4catcode@gmail.com>
2022-12-02 11:42:45 +0800

    simplify the Python script in Challenge 6.

--- quests/6-asset-interop/py-scripts/import-export.py
+++ quests/6-asset-interop/py-scripts/import-export.py
@@ -4,6 +4,7 @@ from stellar_sdk import Network, Keypair, TransactionBuilder
 from stellar_sdk import xdr as stellar_xdr
 from stellar_sdk.soroban import SorobanServer
 from stellar_sdk.soroban.soroban_rpc import TransactionStatus
+from stellar_sdk.soroban_types import InvokerSignature, BigInt, Int64
 
 secret = <SOURCE_ACCOUNT_SECRET>
 rpc_server_url = "<futurenet-horizon-url/soroban/rpc>"
@@ -16,80 +17,16 @@ kp = Keypair.from_secret(secret)
 soroban_server = SorobanServer(rpc_server_url)
 source = soroban_server.load_account(kp.public_key)
 
-def get_nonce() -> stellar_xdr.SCVal:
-    tx = (
-        TransactionBuilder(source, network_passphrase)
-        .set_timeout(300)
-        .append_invoke_contract_function_op(
-            contract_id=contract_id,
-            method="nonce",
-            parameters=[
-                stellar_xdr.SCVal(
-                    stellar_xdr.SCValType.SCV_OBJECT,
-                    obj=stellar_xdr.SCObject(
-                        stellar_xdr.SCObjectType.SCO_VEC,
-                        vec=stellar_xdr.SCVec(
-                            sc_vec=[
-                                stellar_xdr.SCVal(
-                                    stellar_xdr.SCValType.SCV_SYMBOL,
-                                    sym=stellar_xdr.SCSymbol("Account".encode()),
-                                ),
-                                stellar_xdr.SCVal(
-                                    stellar_xdr.SCValType.SCV_OBJECT,
-                                    obj=stellar_xdr.SCObject(
-                                        stellar_xdr.SCObjectType.SCO_ACCOUNT_ID,
-                                        account_id=kp.xdr_account_id(),
-                                    ),
-                                ),
-                            ]
-                        ),
-                    ),
-                ),
-            ],
-            source=kp.public_key,
-        )
-        .build()
-    )
-
-    simulate_transaction_data = soroban_server.simulate_transaction(tx)
-    return stellar_xdr.SCVal.from_xdr(simulate_transaction_data.results[0].xdr)
-
-
-nonce = get_nonce()
-
-source = soroban_server.load_account(
-    kp.public_key
-) # refresh source account, because the current SDK will increment the sequence number by one after building a transaction
-
 tx = (
     TransactionBuilder(source, network_passphrase)
     .set_timeout(300)
     .append_invoke_contract_function_op(
         contract_id=contract_id,
-        method="import", # or 'export'
+        method="import",  # or 'export'
         parameters=[
-            stellar_xdr.SCVal( # Invoker
-                stellar_xdr.SCValType.SCV_OBJECT,
-                obj=stellar_xdr.SCObject(
-                    stellar_xdr.SCObjectType.SCO_VEC,
-                    vec=stellar_xdr.SCVec(
-                        sc_vec=[
-                            stellar_xdr.SCVal(
-                                stellar_xdr.SCValType.SCV_SYMBOL,
-                                sym=stellar_xdr.SCSymbol("Invoker".encode()),
-                            ),
-                        ]
-                    ),
-                ),
-            ),
-            nonce, # Nonce
-            stellar_xdr.SCVal( # amount
-                stellar_xdr.SCValType.SCV_OBJECT,
-                obj=stellar_xdr.SCObject(
-                    stellar_xdr.SCObjectType.SCO_I64,
-                    i64=stellar_xdr.Int64(1_000 * 10**7), # 1,000 tokens
-                ),
-            ),
+            InvokerSignature(),  # Invoker
+            BigInt(0),  # Nonce
+            Int64(1_000 * 10**7)  # amount, 1,000 tokens
         ],
         source=kp.public_key,
     )
